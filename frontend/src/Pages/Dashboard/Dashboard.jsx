f47f8a4bce809e7a6f0fa8e91329b3d96ee79cd1
Vikas Kanoo <vikaskanoozzz@gamil.com>
2023-07-31 10:54:37 +0530

    time is more readable

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -25,7 +25,12 @@ function Dashboard() {
 
     const formatDate = (dateStr)=>{
         const dateObj = new Date(dateStr);
-        return dateObj.toLocaleString();
+        const day = dateObj.getDate().toString().padStart(2, '0');
+        const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+        const year = dateObj.getFullYear();
+        const hours = dateObj.getHours().toString().padStart(2, '0');
+        const minutes = dateObj.getMinutes().toString().padStart(2, '0');
+        return `${day}/${month}/${year}, ${hours}:${minutes}`;
     }
 
   return (
