5981337490a1d1c3bb77f3adec19a1218c62752a
Vikas Kanoo <vikaskanoozzz@gamil.com>
2023-08-01 12:31:54 +0530

    all good

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
 
     const [Forms, setForms] = useState([]);
 
-    const fetchingFunc = async ()=>{
+    const fetchingFunc = async () => {
         try {
             const res = await fetch('https://rentinggoapi.onrender.com/getform');
             const data = await res.json();
@@ -19,11 +19,17 @@ function Dashboard() {
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchingFunc();
-    },[]);
 
-    const formatDate = (dateStr)=>{
+        const intervalId = setInterval(() => {
+            fetchingFunc();
+        }, 10000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const formatDate = (dateStr) => {
         const dateObj = new Date(dateStr);
         const day = dateObj.getDate().toString().padStart(2, '0');
         const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
@@ -33,12 +39,12 @@ function Dashboard() {
         return `${day}/${month}/${year}, ${hours}:${minutes}`;
     }
 
-    const deleteFunc = async (formId)=>{
+    const deleteFunc = async (formId) => {
         try {
             await fetch(`https://rentinggoapi.onrender.com/getform/${formId}`, {
                 method: 'DELETE'
             });
-            setForms((prevForm)=> prevForm.filter((form) => form._id !== formId));
+            setForms((prevForm) => prevForm.filter((form) => form._id !== formId));
             alert('deleted the form by id ' + formId);
         } catch (error) {
             alert('error occured while deleting the form');
@@ -46,37 +52,37 @@ function Dashboard() {
         }
     }
 
-  return (
-    <div>
-        <Header headTxt1="Dash" headTxt2="Board" back="none"/>
-        <div className="DashWrap container-fluid pt-5">
-           <div className="tableHeadingDiv pt-5 border-bottom pb-2">
-              <div className="tableHeading row p-0 ps-3">
-                    <h4 className='col-md-2 text-center p-0'>Product Name</h4>
-                    <h4 className='col-md-2 text-center p-0'>Amount</h4>
-                    <h4 className='col-md-2 text-center p-0'>Number</h4>
-                    <h4 className='col-md-2 text-center p-0'>Date</h4>
-                    <h4 className='col-md-2 text-center p-0'>Date of filling</h4>
-                    <h4 className='col-md-2 text-center p-0'>Call of Action</h4>
-              </div>
-           </div>
-
-           {
-                Forms.map((form, index)=>(
-                    <div key={index} className="tableData row justify-content-between pt-3 ps-3 border-bottom">
-                        <p className="col-md-2 p-0 text-center fs-6">{form.proname}</p>
-                        <p className="col-md-2 p-0 text-center fs-6">{form.amount}</p>
-                        <p className="col-md-2 p-0 text-center fs-6">{form.phoneNo}</p>
-                        <p className="col-md-2 p-0 text-center fs-6">{form.day}</p>
-                        <p className="col-md-2 p-0 text-center fs-6">{formatDate(form.submitDate)}</p>
-                        <p className="col-md-2 p-0 text-center fs-6"><i className="fas fa-check text-primary pe-2 actionIcon"></i> | <i className="fas fa-trash-alt ps-2 text-danger actionIcon" onClick={()=> deleteFunc(form._id)}></i></p>
+    return (
+        <div>
+            <Header headTxt1="Dash" headTxt2="Board" back="none" />
+            <div className="DashWrap container-fluid pt-5">
+                <div className="tableHeadingDiv pt-5 border-bottom pb-2">
+                    <div className="tableHeading row p-0 ps-3">
+                        <h4 className='col-md-2 text-center p-0'>Product Name</h4>
+                        <h4 className='col-md-2 text-center p-0'>Amount</h4>
+                        <h4 className='col-md-2 text-center p-0'>Number</h4>
+                        <h4 className='col-md-2 text-center p-0'>Date</h4>
+                        <h4 className='col-md-2 text-center p-0'>Date of filling</h4>
+                        <h4 className='col-md-2 text-center p-0'>Call of Action</h4>
                     </div>
-                ))
-           }
+                </div>
 
+                {
+                    Forms.map((form, index) => (
+                        <div key={index} className="tableData row justify-content-between pt-3 ps-3 border-bottom">
+                            <p className="col-md-2 p-0 text-center fs-6">{form.proname}</p>
+                            <p className="col-md-2 p-0 text-center fs-6">{form.amount}</p>
+                            <p className="col-md-2 p-0 text-center fs-6">{form.phoneNo}</p>
+                            <p className="col-md-2 p-0 text-center fs-6">{form.day}</p>
+                            <p className="col-md-2 p-0 text-center fs-6">{formatDate(form.submitDate)}</p>
+                            <p className="col-md-2 p-0 text-center fs-6"><i className="fas fa-check text-primary pe-2 actionIcon"></i> | <i className="fas fa-trash-alt ps-2 text-danger actionIcon" onClick={() => deleteFunc(form._id)}></i></p>
+                        </div>
+                    ))
+                }
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Dashboard
